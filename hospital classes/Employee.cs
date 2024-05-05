80f1bdaae7ac7e505bffc32eb4f21c4334181ce8
rouby <abdullahelrouby750@gmail.com>
2024-05-06 01:51:10 +0300

    Bakr code

--- hospital classes/Employee.cs
+++ hospital classes/Employee.cs
@@ -2,4 +2,33 @@ namespace hospital_classes;
 
 public class Employee : Person
 {
+    protected double Salary {get; set; }
+    protected double RaisesAndBouns {get; set; }
+    public int WorkHours { get; set; }
+    public string[] WorkDayes { get; set; }
+    public int Experience { get; set; }
+    public Dictionary<string , string> PreviousExperience { get; set; }
+    protected string HospitalID { get; set; }
+    protected bool SalaryReceived { get; set; }
+    protected string BankAccount { get; set; }
+    protected string AccountNumber { get; set; }
+    static protected int NumberOfEmployees { get; set; }
+
+    //default constructor
+    public Employee()
+    {
+        Salary = 0.0;
+        RaisesAndBouns = 0.0;
+        WorkHours = 0;
+        WorkDayes = new string[7];
+        Experience = 0;
+        PreviousExperience = new Dictionary<string, string>();
+        HospitalID = "xxxxxxxx";
+        SalaryReceived = false;
+        BankAccount = "Bank name";
+        AccountNumber = "0000 0000 0000 0000";
+        NumberOfEmployees++;
+    }
+
+
 }
--- hospital classes/Person.cs
+++ hospital classes/Person.cs
@@ -1,6 +1,29 @@
 ﻿namespace hospital_classes;
 
-public class Person
-{
-    public int id { get; set; }
+public class Person{
+    public string? Firstname { get; set; }
+   public string? Lastname { get; set; }
+   public string? PhoneNumber { get; set; }
+   public int Age { get; set; }
+   public DateOnly DateOfBirth { get; set; }
+   public string? Gender { get; set; }
+   public string? Statue { get; set; }
+   public string? Address { get; set; }
+   public string? BloodType { get; set; }
+
+   //defult constractor 
+   public Person()
+   {
+    Firstname = "First name";
+    Lastname = "Last name";
+    PhoneNumber = "xxxxxxxxxxx";
+    Age = 0;
+    DateOfBirth = DateOnly.FromDateTime(DateTime.Today);
+    Gender = "Unknown";
+    Statue = "Unknown";
+    Address = "Unknown";
+    BloodType = "Unknown";
+   }
+
 }
+
