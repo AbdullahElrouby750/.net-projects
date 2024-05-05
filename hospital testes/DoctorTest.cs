8cc6d48dcbc9b7ec24eae4a5f9835c6d71b3c299
rouby <abdullahelrouby750@gmail.com>
2024-05-05 18:07:39 +0300

    beginning

---
+++ hospital testes/DoctorTest.cs
@@ -0,0 +1,12 @@
+namespace hospital_testes;
+using hospital_classes;
+
+[TestClass]
+public class DoctorTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+}
---
+++ hospital testes/EmployeeTest.cs
@@ -0,0 +1,12 @@
+namespace hospital_testes;
+using hospital_classes;
+
+[TestClass]
+public class EmployeeTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+}
