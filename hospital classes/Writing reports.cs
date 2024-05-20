91f8e9063d980e281d9e8178aa7a14faa2e4c8e8
rouby <abdullahelrouby750@gmail.com>
2024-05-20 18:42:46 +0300

    added other methods

--- hospital classes/Writing reports.cs
+++ hospital classes/Writing reports.cs
@@ -3,4 +3,8 @@ namespace hospital_classes;
 public interface WritingReports
 {
     public void WriteReport();
+    public void PrintHRreport();
+    public void Printsalary();
+    public void login();
+    public void logout();
 }
