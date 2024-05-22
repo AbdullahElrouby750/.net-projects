c021437872cd3e2608b0f4fe45fc94e0807c1253
rouby <abdullahelrouby750@gmail.com>
2024-05-23 00:12:42 +0300

    bakr : finalyyyyyyyyy he finished the new requstes

--- hospital classes/Employee.cs
+++ hospital classes/Employee.cs
@@ -2,25 +2,32 @@ namespace hospital_classes;
 
 public class Employee : Person
 {
+    
     protected double Salary { get; set; }
-    protected double RaisesAndBouns { get; set; }
-    public int WorkHours { get; set; }
-    public string[] WorkDayes { get; set; }
+    protected double Bouns { get; set; }
+    public List<Dictionary<DateOnly, int>> DailyWorkHours { get; set; }
+    public TimeSpan WorkHours { get; set; }
+    protected DateTime ?DailyLoginTime { get; set; }
+    protected DateTime ?DailyLogoutTime { get; set; }
+    protected readonly DateOnly StartingDate; 
+    protected string HRreport { get; set; }
     public int Experience { get; set; }
     public Dictionary<string, string> PreviousExperience { get; set; }
     protected string HospitalID { get; set; }
+    protected string hospitalAccount { get; set; }
     protected bool SalaryReceived { get; set; }
     protected string BankAccount { get; set; }
     protected string AccountNumber { get; set; }
     static protected int NumberOfEmployees { get; set; }
+    protected int Warnings { get; set; }
+    static protected int NumberOfEmployee { get; set; }
 
     //default constructor
-    public Employee()
+    public Employee() 
     {
         Salary = 0.0;
-        RaisesAndBouns = 0.0;
-        WorkHours = 0;
-        WorkDayes = new string[7];
+        Bouns=0.0; 
+        DailyWorkHours = new List< Dictionary<DateOnly,int>>();
         Experience = 0;
         PreviousExperience = new Dictionary<string, string>();
         HospitalID = "xxxxxxxx";
@@ -28,23 +35,25 @@ public class Employee : Person
         BankAccount = "Bank name";
         AccountNumber = "0000 0000 0000 0000";
         NumberOfEmployees++;
+        Warnings = 0;
+        HRreport = "xxxxxxx";
+        hospitalAccount = "  ";
+        NumberOfEmployee = 0;
+        DailyLoginTime =null;
+        DailyLogoutTime = null;
     }
-
-    public Employee(string firstname, string lastname, string phonenumber, int age, DateOnly dateofbirth, string gender, string statue, string address, string bloodtype,
-     double salary, int workHours, string[] workDayes, string hospitalID, string bankAccount, string accountNumber, double raisesAndBouns = 0.0, int experience = 0, Dictionary<string, string> previousExperience = null, bool salaryReceived = false)
-     : base(firstname: firstname, lastname: lastname, phonenumber: phonenumber, age: age, dateofbirth: dateofbirth, gender: gender, statue: statue, address: address, bloodtype: bloodtype)
+    public Employee(Dictionary<string, dynamic> total, double raisesAndBouns = 0.0, bool salaryReceived = false)
+     : base (firstname: total["firstname"], lastname: total["lastname"], phonenumber: total["phonenumber"], age: total["age"], dateofbirth: total["dateofbirth"], gender: total["gender"], statue: total["statue"], address: total["address"], bloodtype: total["bloodtype"])
     {
-        Salary = salary;
-        RaisesAndBouns = raisesAndBouns;
-        WorkHours = workHours;
-        WorkDayes = workDayes;
-        Experience = experience;
-        PreviousExperience = previousExperience;
-        HospitalID = hospitalID;
-        SalaryReceived = salaryReceived;
-        BankAccount = bankAccount;
-        AccountNumber = accountNumber;
-        NumberOfEmployees++;
+        Bouns = total["Bouns"];
+        SalaryReceived=false;
+        Salary = total["salary"];
+        Experience = total["Experience"];
+        HospitalID = total["HospitalID"];
+        BankAccount = total["BankAccount"];
+        AccountNumber = total["AccountNumber"];
+        StartingDate = total["StartingDate"];
+        PreviousExperience = total["PreviousExperience "];
+        DailyWorkHours = total["DailyWorkHours"];
     }
-
 }
